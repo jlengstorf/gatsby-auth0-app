44a2cd88e4be1f496db58234d01a96d0e17644a4
Nick Richmond <nick@pop-os.localdomain>
2019-07-29 10:05:53 -0400

    Destructured props in SessionCheck params

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react"
 import { checkSession } from "./src/utils/auth"
 
-const SessionCheck = props => {
+const SessionCheck = ({ children }) => {
   const [loading, stillLoading] = useState(true);
   useEffect(() => checkSession(() => stillLoading(false)));
-  return loading === false && <>{props.children}</>
+  return loading === false && <>{children}</>
 };
 
 export const wrapRootElement = ({ element }) => (
